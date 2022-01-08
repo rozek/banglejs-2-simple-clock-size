41e82dae1a7157d0e07e11bb2eeed8ee848b7ad1
Andreas Rozek <A.Rozek@gmx.de>
2022-01-08 12:47:20 +0100

    Create simpleClockSize.js

---
+++ simpleClockSize.js
@@ -0,0 +1,18 @@
+(function () {
+  let AppRect;
+
+  exports.CenterX = function CenterX () {
+    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    return AppRect.x + AppRect.w/2;
+  };
+
+  exports.CenterY = function CenterY () {
+    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    return AppRect.y + AppRect.h/2;
+  };
+
+  exports.outerRadius = function outerRadius () {
+    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    return Math.min(AppRect.w/2,AppRect.h/2);
+  };
+})();
