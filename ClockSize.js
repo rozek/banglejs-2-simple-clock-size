0154f36ab1d8a4cb7177e7a489acaa09e41cab0a
Andreas Rozek <A.Rozek@gmx.de>
2022-01-08 13:38:25 +0100

    Update ClockSize.js

--- ClockSize.js
+++ ClockSize.js
@@ -2,17 +2,17 @@
   let AppRect;
 
   exports.CenterX = function CenterX () {
-    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    if (AppRect == null) { AppRect = Bangle.appRect; }
     return AppRect.x + AppRect.w/2;
   };
 
   exports.CenterY = function CenterY () {
-    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    if (AppRect == null) { AppRect = Bangle.appRect; }
     return AppRect.y + AppRect.h/2;
   };
 
   exports.outerRadius = function outerRadius () {
-    if (AppRect == null) { AppRect = Bangle.appRect(); }
+    if (AppRect == null) { AppRect = Bangle.appRect; }
     return Math.min(AppRect.w/2,AppRect.h/2);
   };
 })();
